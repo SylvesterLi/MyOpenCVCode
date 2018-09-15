60f6f9913a93e7435b46325ec065fbe554c359b9
SylvesterLi <lisangyu45@qq.com>
2018-09-15 22:46:02 +0800

    Modified Some Code Blocks

--- OCV3/ocvTest03/main.cpp
+++ OCV3/ocvTest03/main.cpp
@@ -52,12 +52,14 @@ int main(int argc, char** argv) {
 
 	#pragma region ShiTomasi ½Çµã¼ì²â 
 
-	
+	/*
+
 	namedWindow("Good", WINDOW_AUTOSIZE);
 	//ShiTomasi½Çµã¼ì²â  
 	createTrackbar("trackBarName", "Good", &maxCorner, 255, ShiTomasiTrack);
 	ShiTomasiTrack(0, 0);
 	
+	*/
 
 	#pragma endregion
 
@@ -71,6 +73,12 @@ int main(int argc, char** argv) {
 
 	#pragma endregion
 
+	#pragma region SURF ÌØÕ÷¼ì²â
+
+	namedWindow("result", WINDOW_AUTOSIZE);
+	imshow("result", src);
+
+	#pragma endregion
 
 
 	waitKey(0);
